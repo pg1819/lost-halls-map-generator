7f11da912000266ed679c943664ae6c2fee4d652
Prashant Gurung <prashant.grg07@gmail.com>
2023-03-16 16:46:24 +0000

    functions for split branches

--- room.py
+++ room.py
@@ -12,11 +12,15 @@ class Room:
         A room can be at most one type (e.g. a room cannot be a pot room and defender room at the same time).
         At the same time, a room can have no type too.
         """
+
         # Edges
         self.up = False
         self.right = False
         self.down = False
         self.left = False
+
+        # Branch
+        self.main = False
 
         # Types
         self.troom = False
@@ -57,5 +61,23 @@ class Room:
         :return: Nothing
         """
         self.colossus = not self.colossus
+        self.empty = not (self.left or self.right or self.down or self.right or
+                          self.troom or self.pot or self.defender or self.colossus)
+
+    def toggle_troom(self):
+        """
+        Toggles a room to be the treasure room
+        :return: Nothing
+        """
+        self.troom = not self.troom
+        self.empty = not (self.left or self.right or self.down or self.right or
+                          self.troom or self.pot or self.defender or self.colossus)
+
+    def toggle_pot(self):
+        """
+        Toggles a room to be the treasure room
+        :return: Nothing
+        """
+        self.pot = not self.pot
         self.empty = not (self.left or self.right or self.down or self.right or
                           self.troom or self.pot or self.defender or self.colossus)
